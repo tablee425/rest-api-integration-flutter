5bff825a5c0f0bd22f86dbbc9cc32961f4740aad
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-07-31 22:49:48 +0400

    progress dialog and splash page

---
+++ rest_api_integration_flutter/lib/pages/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:rest_api_integration_flutter/utils/constants.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final globalKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
--- rest_api_integration_flutter/lib/pages/splash_page.dart
+++ rest_api_integration_flutter/lib/pages/splash_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:rest_api_integration_flutter/utils/app_shared_preferences.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -12,7 +13,53 @@ class _SplashPageState extends State<SplashPage> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    new Future.delayed(const Duration(seconds: 3), _handleTapEvent);
+    return new Scaffold(
+      key: globalKey,
+      body: _splashContainer(),
+    );
+  }
+
+  Widget _splashContainer() {
+    return GestureDetector(
+      onTap: _handleTapEvent,
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: new BoxDecoration(color: Colors.blue[400]),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            new Container(
+              child: new Image(
+                width: 200,
+                height: 200,
+                image: new AssetImage('assets/images/ic_launcher.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            new Container(
+              margin: EdgeInsets.only(top: 20.0),
+              child: new Text('Flutter Client PHP Backend', style: TextStyle(color: Colors.white, fontSize: 24.0)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleTapEvent() async {
+    bool isLoggedIn = await AppSharedPreferences.isUserLoggedIn();
+    if (this.mounted) {
+      setState(() {
+        if (isLoggedIn != null && isLoggedIn) {
+          // home page
+        } else {
+          // login page
+        }
+      });
+    }
   }
 }
