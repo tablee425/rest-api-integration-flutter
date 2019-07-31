da53cdedbd59362f218edbe71e9b6b15285c969e
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-07-31 20:25:12 +0400

    initialize splash page

---
+++ rest_api_integration_flutter/lib/pages/splash_page.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  final globalKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
